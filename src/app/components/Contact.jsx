01f73bc6777f0ee21e39c7c36ce88346bae0fb43
lauraxavier <laura.ac.xavier@gmail.com>
2024-08-29 17:30:31 -0300

    Change in resume

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -5,201 +5,224 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 const Contact = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
-  const [formValues, setFormValues] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      name: e.target.name.value,
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/hello";
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-      setFormValues({
+    const [emailSubmitted, setEmailSubmitted] = useState(false);
+    const [formValues, setFormValues] = useState({
         name: "",
         email: "",
         subject: "",
         message: "",
-      });
-      setTimeout(() => {
-        setEmailSubmitted(false);
-      }, 2000);
-    } else {
-      console.error("Failed to send message. Status:", response.status);
-    }
-  };
+    });
 
-  return (
-    <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 min-h-screen justify-center items-center"
-    >
-      <div className="z-10">
-        <h5 className="text-xl font-bold my-2">Quer saber mais sobre mim ?</h5>
-        <p className="mb-4 max-w-md">
-          Estou sempre pronta para novas oportunidades! Seja para fazer uma
-          pergunta, trocar ideias ou simplesmente dizer sua opinião. Vamos
-          conversar!
-        </p>
-        <div className="socials flex flex-row gap-4 mt-6">
-          <Link href="https://www.github.com/lauraxavier" target="_blank">
-            <motion.div
-              whileHover={{
-                rotate: [0, -10, 10, -10, 0],
-                transition: { duration: 0.5 },
-              }}
-            >
-              <div className="text-zinc-100">
-                <Image
-                  src="/images/github.png"
-                  width={50}
-                  height={50}
-                  alt="Github Icon"
-                />
-              </div>
-            </motion.div>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/laura-xavier-75b9a0243/"
-            target="_blank"
-          >
-            <motion.div
-              whileHover={{
-                rotate: [0, -10, 10, -10, 0],
-                transition: { duration: 0.5 },
-              }}
-            >
-              <div className="text-zinc-100">
-                <Image
-                  src="/images/linkedin.png"
-                  width={50}
-                  height={50}
-                  alt="Linkedin Icon"
-                />
-              </div>
-            </motion.div>
-          </Link>
-        </div>
-      </div>
-      <div>
-        <form className="flex flex-col mt-10" onSubmit={handleSubmit}>
-          <div className="mb-6">
-            <label htmlFor="name" className="block mb-2 text-sm font-medium">
-              Nome
-            </label>
-            <input
-              name="name"
-              type="text"
-              id="name"
-              required
-              value={formValues.name}
-              onChange={(e) =>
-                setFormValues({ ...formValues, name: e.target.value })
-              }
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Me chamo"
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium">
-              Seu email
-            </label>
-            <input
-              name="email"
-              type="email"
-              id="email"
-              required
-              value={formValues.email}
-              onChange={(e) =>
-                setFormValues({ ...formValues, email: e.target.value })
-              }
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="seu-email@gmail.com"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="subject"
-              className=" block text-sm mb-2 font-medium"
-            >
-              Assunto
-            </label>
-            <input
-              name="subject"
-              type="text"
-              id="subject"
-              required
-              value={formValues.subject}
-              onChange={(e) =>
-                setFormValues({ ...formValues, subject: e.target.value })
-              }
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-              placeholder="Apenas dizendo oi!"
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="message"
-              className=" block text-sm mb-2 font-medium"
-            >
-              Mensagem
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              value={formValues.message}
-              onChange={(e) =>
-                setFormValues({ ...formValues, message: e.target.value })
-              }
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5 h-[180px]"
-              placeholder="Vamos falar sobre..."
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-primary-500 hover:bg-primary-600  font-medium py-2.5 px-5 rounded-lg w-full"
-          >
-            Enviar mensagem
-          </button>
-          {emailSubmitted ? (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.5 }}
-              className="bg-green-100 mt-3 text-green-700 p-2 rounded-md mb-4 justify-center flex text-center"
-            >
-              Mensagem enviada com sucesso!
-            </motion.div>
-          ) : null}
-        </form>
-      </div>
-    </section>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        };
+
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "/api/hello";
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSONdata,
+        };
+
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+
+        if (response.status === 200) {
+            console.log("Message sent.");
+            setEmailSubmitted(true);
+            setFormValues({
+                name: "",
+                email: "",
+                subject: "",
+                message: "",
+            });
+            setTimeout(() => {
+                setEmailSubmitted(false);
+            }, 2000);
+        } else {
+            console.error("Failed to send message. Status:", response.status);
+        }
+    };
+
+    return (
+        <section
+            id="contact"
+            className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 min-h-screen justify-center items-center"
+        >
+            <div className="z-10">
+                <h5 className="text-xl font-bold my-2">
+                    Quer saber mais sobre mim ?
+                </h5>
+                <p className="mb-4 max-w-md">
+                    Estou sempre pronta para novas oportunidades! Seja para
+                    fazer uma pergunta, trocar ideias ou simplesmente dizer sua
+                    opinião. Vamos conversar!
+                </p>
+                <div className="socials flex flex-row gap-4 mt-6">
+                    <Link
+                        href="https://www.github.com/lauraxavier"
+                        target="_blank"
+                    >
+                        <motion.div
+                            whileHover={{
+                                rotate: [0, -10, 10, -10, 0],
+                                transition: { duration: 0.5 },
+                            }}
+                        >
+                            <div className="text-zinc-100">
+                                <Image
+                                    src="/images/github.png"
+                                    width={50}
+                                    height={50}
+                                    alt="Github Icon"
+                                />
+                            </div>
+                        </motion.div>
+                    </Link>
+                    <Link
+                        href="https://www.linkedin.com/in/laura-xavier-75b9a0243/"
+                        target="_blank"
+                    >
+                        <motion.div
+                            whileHover={{
+                                rotate: [0, -10, 10, -10, 0],
+                                transition: { duration: 0.5 },
+                            }}
+                        >
+                            <div className="text-zinc-100">
+                                <Image
+                                    src="/images/linkedin.png"
+                                    width={50}
+                                    height={50}
+                                    alt="Linkedin Icon"
+                                />
+                            </div>
+                        </motion.div>
+                    </Link>
+                </div>
+            </div>
+            <div>
+                <form className="flex flex-col mt-10" onSubmit={handleSubmit}>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="name"
+                            className="block mb-2 text-sm font-medium"
+                        >
+                            Nome
+                        </label>
+                        <input
+                            name="name"
+                            type="text"
+                            id="name"
+                            required
+                            value={formValues.name}
+                            onChange={(e) =>
+                                setFormValues({
+                                    ...formValues,
+                                    name: e.target.value,
+                                })
+                            }
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="Me chamo"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium"
+                        >
+                            Seu email
+                        </label>
+                        <input
+                            name="email"
+                            type="email"
+                            id="email"
+                            required
+                            value={formValues.email}
+                            onChange={(e) =>
+                                setFormValues({
+                                    ...formValues,
+                                    email: e.target.value,
+                                })
+                            }
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="seu-email@gmail.com"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="subject"
+                            className=" block text-sm mb-2 font-medium"
+                        >
+                            Assunto
+                        </label>
+                        <input
+                            name="subject"
+                            type="text"
+                            id="subject"
+                            required
+                            value={formValues.subject}
+                            onChange={(e) =>
+                                setFormValues({
+                                    ...formValues,
+                                    subject: e.target.value,
+                                })
+                            }
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="Apenas dizendo oi!"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="message"
+                            className=" block text-sm mb-2 font-medium"
+                        >
+                            Mensagem
+                        </label>
+                        <textarea
+                            name="message"
+                            id="message"
+                            value={formValues.message}
+                            onChange={(e) =>
+                                setFormValues({
+                                    ...formValues,
+                                    message: e.target.value,
+                                })
+                            }
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5 h-[180px]"
+                            placeholder="Vamos falar sobre..."
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-primary-500 hover:bg-primary-600  font-medium py-2.5 px-5 rounded-lg w-full"
+                    >
+                        Enviar mensagem
+                    </button>
+                    {emailSubmitted ? (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className="bg-green-100 mt-3 text-green-700 p-2 rounded-md mb-4 justify-center flex text-center"
+                        >
+                            Mensagem enviada com sucesso!
+                        </motion.div>
+                    ) : null}
+                </form>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
--- src/app/components/HeaderSection.jsx
+++ src/app/components/HeaderSection.jsx
@@ -8,119 +8,128 @@ const mobileWts = "https://wa.me/5548991574820";
 const webWts = "https://web.whatsapp.com/send?phone=+555548991574820";
 
 function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-  });
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+    });
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-        });
-      }
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            function handleResize() {
+                setWindowSize({
+                    width: window.innerWidth,
+                });
+            }
 
-      window.addEventListener("resize", handleResize);
+            window.addEventListener("resize", handleResize);
 
-      handleResize();
+            handleResize();
 
-      return () => window.removeEventListener("resize", handleResize);
-    }
-  }, []);
-  return windowSize;
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+    return windowSize;
 }
 
 const HeaderSection = () => {
-  const size = useWindowSize();
+    const size = useWindowSize();
 
-  return (
-    <section className="min-h-screen mb-[150px] flex justify-center items-center">
-      <div className="grid grid-cols-1 sm:grid-cols-12 min-h-[510px]">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8 }}
-          className="col-span-12 sm:col-span-12 sm:text-center sm:justify-center order-2 md:order-1 sm:order-1 md:justify-center w-full min-h-[325px]"
-        >
-          <h1 className="mb-2 mt-10 lg:mt-0 min-h-[460px] lg:min-h-[360px] text-zinc-100 text-[35px] sm:text-[50px] md:text-[40px] lg:text-[60px] lg:leading-normal font-extrabold text-center  ">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Olá! Sua empresa está em busca de uma desenvolvedora Front End
-            </span>
-            <br></br>
-            <TypeAnimation
-              sequence={[
-                "Com experiência em React.",
-                2000,
-                "Com conhecimento em HTML, CSS e JavaScript.",
-                2000,
-                "Com conhecimento de Design.",
-                2000,
-                "Capacitada para resolver problemas.",
-                2000,
-                "Ótima em Colaboração e Comunicação.",
-                2000,
-                "Disposta á aprender sempre.",
-                2000,
-                "Cuidadosa com detalhes de design.",
-                2000,
-                "Com capacidade de gerenciar o tempo .",
-                2000,
-              ]}
-              wrapper="span"
-              speed={70}
-              repeat={Infinity}
-            />
-          </h1>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-12 sm:col-span-12  lg:mt-0 order-1 sm:order-2 flex flex-col lg:flex-row items-center justify-around"
-        >
-          <div className="rounded-full w-[350px] h-[350px] lg:w-[350px] lg:h-[350px] relative mb-[40px] mt-[50px] flex justify-center items-center">
-            <Image
-              src="/images/avatar2.png"
-              alt="avatar"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={800}
-              height={800}
-            />
-          </div>
-          <div className="mb-3 lg:mb-0 justify-center items-center flex flex-col md:flex-row md:items-baseline md:gap-4 lg:gap-4">
-            <motion.a
-              href={size.width < 700 ? mobileWts : webWts}
-              target="_blank"
-              className="px-5 inline-block py-2 w-full max-w-[210px] sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 text-white"
-              whileHover={{
-                scale: 1.2,
-                rotate: [0, 10, -10, 5, -5, 0],
-                backgroundColor: "#4B5563",
-              }}
-              transition={{ duration: 0.5, type: "spring", stiffness: 150 }}
-            >
-              Fale comigo
-            </motion.a>
-            <motion.a
-              href="https://drive.google.com/file/d/14CBT1fFYJYMXwTJghZKVvu8G4H5DjhwJ/view?usp=drive_link"
-              className="px-1 inline-block py-1 w-full max-w-[210px] sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 text-white mt-3"
-              whileHover={{
-                scale: 1.2,
-                rotate: [0, 10, -10, 5, -5, 0],
-                backgroundColor: "#4B5563",
-              }}
-              transition={{ duration: 0.5, type: "spring", stiffness: 150 }}
-              target="_blank"
-            >
-              <span className="block bg-[#121212] text-center hover:bg-slate-800 rounded-full px-4 py-1">
-                Currículo
-              </span>
-            </motion.a>
-          </div>
-        </motion.div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="min-h-screen mb-[150px] flex justify-center items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-12 min-h-[510px]">
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.8 }}
+                    className="col-span-12 sm:col-span-12 sm:text-center sm:justify-center order-2 md:order-1 sm:order-1 md:justify-center w-full min-h-[325px]"
+                >
+                    <h1 className="mb-2 mt-10 lg:mt-0 min-h-[460px] lg:min-h-[360px] text-zinc-100 text-[35px] sm:text-[50px] md:text-[40px] lg:text-[60px] lg:leading-normal font-extrabold text-center  ">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+                            Olá! Sua empresa está em busca de uma desenvolvedora
+                            Front End
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                "Com experiência em React.",
+                                2000,
+                                "Com conhecimento em HTML, CSS e JavaScript.",
+                                2000,
+                                "Com conhecimento de Design.",
+                                2000,
+                                "Capacitada para resolver problemas.",
+                                2000,
+                                "Ótima em Colaboração e Comunicação.",
+                                2000,
+                                "Disposta á aprender sempre.",
+                                2000,
+                                "Cuidadosa com detalhes de design.",
+                                2000,
+                                "Com capacidade de gerenciar o tempo .",
+                                2000,
+                            ]}
+                            wrapper="span"
+                            speed={70}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="col-span-12 sm:col-span-12  lg:mt-0 order-1 sm:order-2 flex flex-col lg:flex-row items-center justify-around"
+                >
+                    <div className="rounded-full w-[350px] h-[350px] lg:w-[350px] lg:h-[350px] relative mb-[40px] mt-[50px] flex justify-center items-center">
+                        <Image
+                            src="/images/avatar2.png"
+                            alt="avatar"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={800}
+                            height={800}
+                        />
+                    </div>
+                    <div className="mb-3 lg:mb-0 justify-center items-center flex flex-col md:flex-row md:items-baseline md:gap-4 lg:gap-4">
+                        <motion.a
+                            href={size.width < 700 ? mobileWts : webWts}
+                            target="_blank"
+                            className="px-5 inline-block py-2 w-full max-w-[210px] sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 text-white"
+                            whileHover={{
+                                scale: 1.2,
+                                rotate: [0, 10, -10, 5, -5, 0],
+                                backgroundColor: "#4B5563",
+                            }}
+                            transition={{
+                                duration: 0.5,
+                                type: "spring",
+                                stiffness: 150,
+                            }}
+                        >
+                            Fale comigo
+                        </motion.a>
+                        <motion.a
+                            href="https://drive.google.com/file/d/1m7O3UUf66GmHUJGWM6PO_PbaQQU3Xz_K/view?usp=sharing"
+                            className="px-1 inline-block py-1 w-full max-w-[210px] sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 text-white mt-3"
+                            whileHover={{
+                                scale: 1.2,
+                                rotate: [0, 10, -10, 5, -5, 0],
+                                backgroundColor: "#4B5563",
+                            }}
+                            transition={{
+                                duration: 0.5,
+                                type: "spring",
+                                stiffness: 150,
+                            }}
+                            target="_blank"
+                        >
+                            <span className="block bg-[#121212] text-center hover:bg-slate-800 rounded-full px-4 py-1">
+                                Currículo
+                            </span>
+                        </motion.a>
+                    </div>
+                </motion.div>
+            </div>
+        </section>
+    );
 };
 
 export default HeaderSection;
